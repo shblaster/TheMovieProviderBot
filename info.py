8da4be42d8e22594460fb00d1c037342400852e0
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2024-06-21 20:01:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/Howto_openthislink/8') # 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'ziplinker.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'edad27b3c1e52fa0b8ce835b2c7af9fe3ceba76b')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
