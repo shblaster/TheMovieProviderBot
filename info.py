35afa168889438019401e7d9e6632d466a32dd08
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2024-06-21 09:31:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,12 +21,12 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/65fe86fc02a73f6fcf0ce.jpg https://telegra.ph/file/6fa70325813885809a64a.jpg https://telegra.ph/file/e06afc1e7abbcd8d4213a.jpg https://telegra.ph/file/3f4040b320d9b7840200a.jpg https://telegra.ph/file/3950fad740fb8ea894df7.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://telegra.ph/file/b6ae7205a9de7369ac42f.jpg https://telegra.ph/file/cd95258ac6d77c5e82854.jpg https://telegra.ph/file/4281fa620c989610be53a.jpg https://telegra.ph/file/bd8814f412cfa4638b7e7.jpg https://telegra.ph/file/7a559089f10fd2045c148.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/86b7b7e2aa7e38f328902.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8169830d821.jpg'))
-CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/7b334ca625c9a995ec3d6.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/e5468266c677761b05bae.jpg")
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/af9db066e220f13dc731b.jpg'))
+CODE = (environ.get('CODE', 'https://telegra.ph/file/044e29c96899f670d9c6c.jpg'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
